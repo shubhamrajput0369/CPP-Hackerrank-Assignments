002e1b30c369c28bb623fc6d30411cd47cc68557
Shubham Rajput <62853440+shubhamrajput0369@users.noreply.github.com>
2021-02-06 19:34:13 +0530

    Add files via upload
    Concept of array

---
+++ 8. Array_Introduction.cpp
@@ -0,0 +1,22 @@
+//Author: Shubham B. Rajput
+//C++
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        for(int i=n-1;i>=0;i--)
+        cout<<arr[i]<<" "; 
+          
+    return 0;
+}
+
