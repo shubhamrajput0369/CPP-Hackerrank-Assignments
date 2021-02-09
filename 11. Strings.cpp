429be296ab620b80d827175139d1992d0f738d7b
Shubham Rajput <62853440+shubhamrajput0369@users.noreply.github.com>
2021-02-09 17:50:44 +0530

    Add files via upload
    String concept

---
+++ 11. Strings.cpp
@@ -0,0 +1,22 @@
+//Author: Shubham Bhagwansing Rajput
+//C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a,b;
+    char t;
+    cin>>a>>b;
+    
+    cout<<a.size()<<" "<<b.size();
+    cout<<"\n"<<a+b;
+    t=a[0];
+    a[0]=b[0];
+    b[0]=t;
+    cout<<"\n"<<a<<" "<<b;
+    
+    return 0;
+}
+
