6865160dfb35fc96a383bee24b06db0fd9a253ad
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-02-21 14:49:22 +0530

    Add files via upload
    Maps concept.

---
+++ 2.8 Maps-STL.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    map<string,int>m;
+    int Q,qry;
+    string str;
+    cin>>Q;
+    
+    for(int i=0, tmp;i<Q;i++)
+    {   
+        cin>>qry;
+        cin>>str;
+        if(qry==1)
+        {
+            cin>>tmp;
+            m[str]=tmp+m[str];
+        }
+        
+        if(qry==2)
+            m[str]=0;
+        
+        if(qry==3)    
+            cout<<m[str]<<"\n";
+    }   
+    return 0;
+}
+
